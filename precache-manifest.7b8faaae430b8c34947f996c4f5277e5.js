1f91fc151af1bb287eda4dc9ee27720d7df627c1
websjc <josepconesabotia@gmail.com>
2019-05-15 11:58:08 +0200

    primer commit

---
+++ precache-manifest.7b8faaae430b8c34947f996c4f5277e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abf970d702dc1a76ffc8",
+    "url": "/static/js/main.0c2641cf.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "abf970d702dc1a76ffc8",
+    "url": "/static/css/main.a5c270f9.chunk.css"
+  },
+  {
+    "revision": "f1a7e846b8cc9aafd33cbcf5ebfc493d",
+    "url": "/index.html"
+  }
+];
